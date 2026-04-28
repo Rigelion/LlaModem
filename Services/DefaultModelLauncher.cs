3fc2864825700b43601c3aa8d66732b99d370111
Rigelion <dino.habibovic@gmail.com>
2026-04-28 10:11:58 +0200

    Expand environment variable placeholders in script paths
    Use Environment.ExpandEnvironmentVariables to resolve %VAR% and ${VAR}
    placeholders in StartScript values, so config can reference env vars.

--- Services/DefaultModelLauncher.cs
+++ Services/DefaultModelLauncher.cs
@@ -6,6 +6,7 @@ public class DefaultModelLauncher : IModelLauncher
 {
     public async Task<Process?> StartAsync(string scriptPath)
     {
+        scriptPath = Environment.ExpandEnvironmentVariables(scriptPath);
         var workingDir = Path.GetDirectoryName(scriptPath);
 
         var psi = new ProcessStartInfo
