53bce332402a20e95899636cd261525351aeaf8c
Rigelion <dino.habibovic@gmail.com>
2026-05-11 18:21:54 +0200

    Use New-Object PSObject for proper property access

--- powershell/config.ps1
+++ powershell/config.ps1
@@ -1,7 +1,7 @@
 # Centralized configuration for llama-server scripts
 # Modify paths here to change all scripts at once
 
-$script:CachePaths = [PSCustomObject]@{
+$script:CachePaths = New-Object -TypeName PSObject -Property @{
     Temp = "F:\Temp"
     LlamaCache = "F:\llama-cache"
     HfCache = "F:\hf-cache"
